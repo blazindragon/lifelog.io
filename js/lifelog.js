75517f03e62ae565f8f92536a0a16878236dd476
Faraaz Sareshwala <fsareshwala@lifelog.io>
2013-10-14 03:02:02 +0000

    index.html: call showdown.js showdown.min.js

--- js/lifelog.js
+++ js/lifelog.js
@@ -28,11 +28,22 @@ app.config(function($httpProvider) {
                     response.data = converter.makeHtml(response.data);
                 }
 
-                console.log(response);
                 return response;
             }
         }
     });
+});
+
+app.directive('markdown', function() {
+    var converter = new Showdown.converter();
+
+    return {
+        restrict: 'E',
+        link: function(scope, element, attrs) {
+            var html = converter.makeHtml(element.text());
+            element.html(html);
+        }
+    }
 });
 
 app.filter('splitTag', function() {
