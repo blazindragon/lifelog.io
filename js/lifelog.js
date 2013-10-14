b90936bd6648e94c60d748aa6ac2175f88df9b80
Faraaz Sareshwala <fsareshwala@lifelog.io>
2013-10-14 03:01:53 +0000

    main: revert heading back to pure html

--- js/lifelog.js
+++ js/lifelog.js
@@ -14,16 +14,9 @@ app.config(function($routeProvider) {
     otherwise({redirectTo:'/'});
 });
 
-app.directive('markdown', function() {
     var converter = new Showdown.converter();
 
-    return {
-        restrict: 'E',
-        link: function(scope, element, attrs) {
-            var html = converter.makeHtml(element.text());
-            element.html(html);
         }
-    }
 });
 
 app.filter('splitTag', function() {
